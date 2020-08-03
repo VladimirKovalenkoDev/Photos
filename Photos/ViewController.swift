b853eba3368f40abd40685aa5131280577bde55f
Vladimir Kovalenko <vovkazon@mail.ru>
2020-08-03 13:49:28 +0300

    Initial Commit

---
+++ Photos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photos
+//
+//  Created by Владимир Коваленко on 03.08.2020.
+//  Copyright © 2020 Vladimir Kovalenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
